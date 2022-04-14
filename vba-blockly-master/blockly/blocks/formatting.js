be0fce08bf75dea8431186f67eefc0f691e9fdb1
James Humphreys <j.humphreys1@lancaster.ac.uk>
2022-04-14 14:18:08 +0100

    Added some new files to set up for adding the formatting category.Currently having issues getting the actual category to open for some reason, need to keep an eye out!

---
+++ vba-blockly-master/blockly/blocks/formatting.js
@@ -0,0 +1,22 @@
+//This file will be used to add formatting options
+
+'use strict';
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly');
+
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "wrap_text",
+        "message0": "Wrap Text",
+        "output": null,
+        "colour": 0,
+        "tooltip": "Wraps text onto a new line in the same cell",
+        "helpUrl": ""
+    }
+
+
+
+
+
+])
---
+++ vba-blockly-master/blockly/generators/VBA/formatting.js
@@ -0,0 +1,10 @@
+'use strict';
+
+goog.provide('Blockly.VBA.Formattings');
+
+goog.require('Blockly.VBA');
+
+Blockly.VBA['wrap_text'] = function (block) {
+    var code = '.WrapText';
+    return code;
+}
